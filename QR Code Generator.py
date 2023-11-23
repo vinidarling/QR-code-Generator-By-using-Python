95c58b50300921eca94cda1f5697e3795731ccc0
vinidarling <151606607+vinidarling@users.noreply.github.com>
2023-11-23 21:55:10 +0530

    Add files via upload

---
+++ QR Code Generator.py
@@ -0,0 +1,16 @@
+
+import qrcode
+
+data = "vineeth"
+
+
+qr = qrcode.QRCode(version = 1,
+				box_size = 10,
+				border = 5)
+
+qr.add_data(data)
+
+qr.make(fit = True)
+img = qr.make_image(fill_color = 'red',back_color = 'white')
+
+img.save('MyQRCode.png')
